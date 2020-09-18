101aff33f15f819cb6d1d53648c6c7cc3f2f7c9c
Simone Scalamandrè <simone.scalamandre95@gmail.com>
2020-09-18 17:38:47 +0200

    Added mocking function

--- tests/test_mocking.py
+++ tests/test_mocking.py
@@ -17,10 +17,35 @@ def test_mocking():
 
 
 def test_raise_missing_mocking():
+    @mocking([
+        ((7, 8, 9), {"c": 3}, 3)
+    ])
+    def a():
+        pass
 
     @mocking([])
-    def a():
+    def b():
         pass
 
     with pytest.raises(KeyError):
         a(1)
+
+    with pytest.raises(KeyError):
+        a(7, 8, 9, c=1)
+
+
+def test_raise_missing_mocking_parameters():
+
+    with pytest.raises(TypeError):
+        @mocking([
+            ((7, 8, 9), {"c": 3})
+        ])
+        def a():
+            pass
+
+    with pytest.raises(TypeError):
+        @mocking([
+            ({7, 8, 9}, {"c": 3})
+        ])
+        def a():
+            pass
--- typeguard/_mocking.py
+++ typeguard/_mocking.py
@@ -1,7 +1,51 @@
 from functools import wraps
+from ._checktype import checktype
+
+value_handler = []
 
 
-def mocking(fn):
-    @wraps(fn)
-    def wrapper(*args, **kwargs):
-        pass
+@checktype
+def check_mock(args: tuple, kwargs: dict, retr):
+    return
+
+
+def save_mock(mock: tuple):
+    if mock not in value_handler:
+        value_handler.append(mock)
+    else:
+        raise KeyError(f"Tuple {mock} added yet")
+
+
+def is_mock_matching(args_tuple, value_handler): #, current_tuple):
+    args, kwargs = args_tuple
+    #print("args: ", args, " - KWARGS: ", kwargs)
+    for tuple_handler in value_handler:
+        #print("CHECK: ", tuple_handler)
+        if args in tuple_handler and kwargs in tuple_handler:
+            #print("TUPLA TROVATA: ", tuple_handler)
+            return tuple_handler
+    return None
+
+
+def mocking(mocks: list):
+    # Controllo la validità della tupla inserita come parametro
+    for mock in mocks:
+        check_mock(*mock)
+        # Salvo nella lista le tuple passate
+        save_mock(mock)
+
+    def fn_wrapper(fn):
+
+        @wraps(fn)
+        def arg_wrapper(*args, **kwargs):
+            print("value_handler: ", value_handler)
+
+            tuple = is_mock_matching((args, kwargs), value_handler)
+            if tuple:
+                return tuple[2]
+            else:
+                raise KeyError("Unmatched parameters: ", (args, kwargs), " -> choose between: ", value_handler)
+
+        return arg_wrapper
+
+    return fn_wrapper
