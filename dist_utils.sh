1f2e519c93df45cfeba05221d0ba3d13ea0bae6f
Simone Scalamandrè <simone.scalamandre95@gmail.com>
2020-09-22 14:13:13 +0200

    Added script to upload packages

---
+++ dist_utils.sh
@@ -0,0 +1,22 @@
+# Installing "setuptools" and "wheel" libraries
+python3 -m pip install --user --upgrade setuptools wheel
+
+# Creating dist archives
+python3 setup.py sdist bdist_wheel
+
+# Upgrade "twine" module
+python3 -m pip install --user --upgrade twine
+
+read -p "Enter environment to upload package[dev, pro]: " environment
+
+if [ $environment == "dev" ]
+then
+  # Upload dists archives to "testpypi"
+  python3 -m twine upload --repository testpypi dist/*
+elif [ $environment == "pro" ]
+then
+  # Upload dists archives to "testpypi"
+  python3 -m twine upload dist/*
+else
+  echo "Environment not found, choose between [\"dev\", \"pro\"]"
+fi
