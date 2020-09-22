415419c8c56ee023836419e12d0d5a89f2fbc030
Simone Scalamandrè <simone.scalamandre95@gmail.com>
2020-09-22 15:26:51 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-#from distutils.core import setup
 import setuptools
 
 with open("README.md", "r") as fh:
@@ -6,19 +5,19 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="decoratorutilities",
-    version="1.0",
-    author="Giovanni Cardamone, Scalamandrè Simone",
-    author_email="giovanni.cardamone@sourcesense.com, simone.scalamandre@sourcesense.com",
+    version="1.0.0",
+    author="Scalamandrè Simone, Giovanni Cardamone",
+    author_email="simone.scalamandre@sourcesense.com, giovanni.cardamone@sourcesense.com",
     description="Python library to user type guard utilities to check parameters and return type, allow function overloading and function mocking at runtime",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    url="https://github.com/Symone95/decoratorutilities",
+    packages=["decoratorutilities"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='3.7',
 
 )
