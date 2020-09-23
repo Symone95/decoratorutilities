9aa9aae95b02b547f4fae200fde1cce8f46bca33
Simone Scalamandrè <simone.scalamandre95@gmail.com>
2020-09-23 17:29:18 +0200

    rst fix

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 
 project = 'decoratorutilities'
 copyright = '2020, Simone'
-author = 'Simone'
+author = 'Simone95'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
@@ -39,15 +39,27 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['Thumbs.db', '.DS_Store']
 
+# The master toctree document.
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+html_theme_options = {
+    'collapse_navigation': True,
+    'display_version': True,
+    'navigation_depth': 3,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
